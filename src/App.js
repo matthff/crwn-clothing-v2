0c71f8a9fa2843b008dbb58e1d7b3f446ee0c6d2
unknown <matheuspsnbrasil@hotmail.com>
2022-04-02 17:34:30 -0300

    Navbar and Homepage w/ styling

--- src/App.js
+++ src/App.js
@@ -1,43 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+import Navigation from "./routes/navigation/navigation.component.jsx";
+import Home from "./routes/home/home.component.jsx";
+import Shop from "./routes/shop/shop.component.jsx";
+
 const App = () => {
-  return (
-    <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Hats</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-    </div>
-  );
+  return(
+    <Routes>
+      <Route path="/" element={<Navigation/>}>
+        <Route index element={<Home/>}/>
+        <Route path="/shop" element={<Shop/>}/>
+      </Route>
+    </Routes>
+  )
 };
 
 export default App;
