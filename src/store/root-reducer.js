4eb7cfced3b00e1764036d0f561d04cea7c48bbc
unknown <matheuspsnbrasil@hotmail.com>
2022-05-03 23:28:07 -0300

    added redux persist

--- src/store/root-reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux"; 
-
-import { userReducer } from "./user/user.reducer.js";
-
-export const rootReducer = combineReducers({
-    user: userReducer,
-});
--- src/store/store.js
+++ src/store/store.js
@@ -1,22 +1,47 @@
+import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
+import { 
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER, 
+} from 'redux-persist';
 
 import userReducer from './user/user.slice.js';
 import categoriesReducer from './categories/categories.slice.js';
 import cartReducer from './cart/cart.slice.js';
 
+const reducers = combineReducers({
+    user: userReducer,
+    categories: categoriesReducer,
+    cart: cartReducer
+});
 
-export const store = configureStore({
-    reducer: {
-        user: userReducer,
-        categories: categoriesReducer,
-        cart: cartReducer
-    },
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+    blacklist: ['user'],
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+    reducer: persistedReducer,
     middleware: getDefaultMiddleware => getDefaultMiddleware({
         serializableCheck: {
-            ignoredActions: ['user/currentUser'],
+            ignoredActions: ['setCurrentUser', FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
             ignoredActionPaths: ['payload'],
             ignoredPaths: ['user.currentUser']
         },
-    }).concat(logger),
+    }).concat(middleWares),
 });
+
+export default store;
